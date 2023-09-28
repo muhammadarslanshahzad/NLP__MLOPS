eb27d29769714ec646924f3d451997814ed38ab8
marslanshahzad <marslanshahzad14@gmail.com>
2023-09-28 06:07:39 +0500

    setting up exceptions

---
+++ src/TextSummerizer/utils/common.py
@@ -0,0 +1,72 @@
+'''
+    The all the commons functions used in whole projects are defined here
+'''
+
+import os
+from ensure import ensure_annotations
+from box.exceptions import BoxValueError
+import yaml
+from textSummerizer import logger
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path)-> ConfigBox:
+    '''
+        read yaml file and returns
+
+        Args: 
+            path_to_yaml (str): path like input
+
+        Raises: 
+            ValueError: if yaml file is empty
+            e: empty file error
+
+        Returns:
+            ConfigBox: ConfigBox type 
+    '''
+
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"***************\nyaml file: {path_to_yaml} loaded successfully!\n\n\n")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError(f"yaml file {path_to_yaml} is empty")
+    except Exception as e:
+        raise e
+    
+
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    '''
+        create list of directories 
+
+        Args: 
+            path_to_directories (list): list to the path of directories
+            ignore_log(bool , optional): ignore if multiple dirs is to be created defaults to False.
+
+    '''
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f'creating directory at: {path}')
+
+        
+
+@ensure_annotations
+def get_size(path:Path)->str:
+    '''
+        get size in KB
+
+        Args:
+            path (Path): path of the file
+
+        Returns:
+            str: size in KB 
+    '''
+
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f"~{size_in_kb} KB"
